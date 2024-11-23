0a3f8427e4f3ceeb804302637a4827164e36f462
SergioCainzo <sergiocainzo@gmail.com>
2024-11-23 08:38:55 -0300

    Configurando controle

--- src/main/java/br/com/projeto/api/controller/ClienteController.java
+++ src/main/java/br/com/projeto/api/controller/ClienteController.java
@@ -1,5 +1,16 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("")
 public class ClienteController {
     
+    @GetMapping("/")
+    public String teste(){
+        return "Hello World!!!";
+    }
+
 }
