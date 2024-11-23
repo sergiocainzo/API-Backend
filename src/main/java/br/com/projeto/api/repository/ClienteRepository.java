8a1fa46dca229b2c9a6a21fb3a742333e5c9b9fb
SergioCainzo <sergiocainzo@gmail.com>
2024-11-23 08:47:41 -0300

    Configurando Repository

--- src/main/java/br/com/projeto/api/repository/ClienteRepository.java
+++ src/main/java/br/com/projeto/api/repository/ClienteRepository.java
@@ -1,6 +1,11 @@
 package br.com.projeto.api.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ClienteRepository {
+import br.com.projeto.api.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
     
 }
