c60ad0f48af0852b3db4c0ff4d0b925e7f587507
SergioCainzo <sergiocainzo@gmail.com>
2024-11-23 09:28:07 -0300

    Rota de edição

--- src/main/java/br/com/projeto/api/controller/ClienteController.java
+++ src/main/java/br/com/projeto/api/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.projeto.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,10 @@ public class ClienteController {
         return acao.findAll();
     }
 
+    @PutMapping("/")
+    public Cliente editar(@RequestBody Cliente c){
+        return acao.save(c);
+    }
 
 
 
