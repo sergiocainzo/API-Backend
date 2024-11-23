bc72fa612f02052671d5ec6950be8841faa82697
SergioCainzo <sergiocainzo@gmail.com>
2024-11-23 09:20:23 -0300

    Rota de cadastro

--- src/main/java/br/com/projeto/api/controller/ClienteController.java
+++ src/main/java/br/com/projeto/api/controller/ClienteController.java
@@ -2,23 +2,34 @@ package br.com.projeto.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.api.model.Cliente;
 import br.com.projeto.api.repository.ClienteRepository;
 
 @RestController
-@RequestMapping("")
+@RequestMapping
 public class ClienteController {
     
     @Autowired
     private ClienteRepository acao;
 
 
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c){
+        return acao.save(c);
+    }
 
     @GetMapping("/helloworld")
     public String teste(){
         return "Hello World!!!";
     }
+
+
+
+
 
 }
