6def1fddd6d36892db744fea1a79e00548692596
SergioCainzo <sergiocainzo@gmail.com>
2024-11-23 08:44:44 -0300

    Configurando model

--- src/main/java/br/com/projeto/api/controller/ClienteController.java
+++ src/main/java/br/com/projeto/api/controller/ClienteController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("")
+@RequestMapping("")
 public class ClienteController {
     
-    @GetMapping("/")
+    @GetMapping("/helloworld")
     public String teste(){
         return "Hello World!!!";
     }
--- src/main/java/br/com/projeto/api/model/Cliente.java
+++ src/main/java/br/com/projeto/api/model/Cliente.java
@@ -1,5 +1,22 @@
 package br.com.projeto.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "clientes")
+@Data
 public class Cliente {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String nome;
+    private int idade;
+    private String cidade;
+
 }
