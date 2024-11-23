0655eb5123212ab7531dc8a16f649dd3d21f9e3b
SergioCainzo <sergiocainzo@gmail.com>
2024-11-23 09:14:20 -0300

    Annotation @Autowired

--- src/main/java/br/com/projeto/api/controller/ClienteController.java
+++ src/main/java/br/com/projeto/api/controller/ClienteController.java
@@ -1,13 +1,21 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("")
 public class ClienteController {
     
+    @Autowired
+    private ClienteRepository acao;
+
+
+
     @GetMapping("/helloworld")
     public String teste(){
         return "Hello World!!!";
