9357dfc5f879b92daa0518e9fd57adc49d7c134a
SergioCainzo <sergiocainzo@gmail.com>
2024-11-23 09:41:25 -0300

    CORS

--- src/main/java/br/com/projeto/api/controller/ClienteController.java
+++ src/main/java/br/com/projeto/api/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.projeto.api.repository.ClienteRepository;
 
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "*")
 public class ClienteController {
     
     @Autowired
